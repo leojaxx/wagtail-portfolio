c8fad375269ba422e08062c0fbd151685fd43ccc
Leonard Jackson <rupyhacker@gmail.com>
2017-02-06 14:08:17 -0700

    edited some old settings

--- portfolio/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ENV = os.getenv('PORTFOLIO_ENV', 'dev')
-
-if ENV == 'dev':
-    from .dev import *
-elif ENV == 'prod':
-    from .production import *
--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+from __future__ import absolute_import, unicode_literals
+
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.production")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-env=PORTFOLIO_ENV='prod'
+os.environ["DJANGO_SETTINGS_MODULE"] = 'wagtail-portfolio.settings.production'
+
+application = get_wsgi_application()
